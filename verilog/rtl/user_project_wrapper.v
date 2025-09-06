c9064116f2a2a20d7384c16f0870bbcce12945b0
Samarthjainabout <30805083+Samarthjainabout@users.noreply.github.com>
2025-09-06 18:53:05 +0530

    updated user_project_wrapper
    with power instantiations

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -1,106 +1,97 @@
-// SPDX-FileCopyrightText: 2020 Efabless Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// SPDX-License-Identifier: Apache-2.0
-
 `default_nettype none
-/*
- *-------------------------------------------------------------
- *
- * user_project_wrapper
- *
- * This wrapper enumerates all of the pins available to the
- * user for the user project.
- *
- * An example user project is provided in this wrapper.  The
- * example should be removed and replaced with the actual
- * user project.
- *
- *-------------------------------------------------------------
- */
-
 module user_project_wrapper #(
     parameter BITS = 32
 ) (
 `ifdef USE_POWER_PINS
-    inout vdda1,	// User area 1 3.3V supply
-    inout vdda2,	// User area 2 3.3V supply
-    inout vssa1,	// User area 1 analog ground
-    inout vssa2,	// User area 2 analog ground
-    inout vccd1,	// User area 1 1.8V supply
-    inout vccd2,	// User area 2 1.8v supply
-    inout vssd1,	// User area 1 digital ground
-    inout vssd2,	// User area 2 digital ground
+    inout vdda1, inout vdda2,
+    inout vssa1, inout vssa2,
+    inout vccd1, inout vccd2,
+    inout vssd1, inout vssd2,
 `endif
 
-    // Wishbone Slave ports (WB MI A)
-    input wb_clk_i,
-    input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
+    // Wishbone
+    input         wb_clk_i,
+    input         wb_rst_i,
+    input         wbs_stb_i,
+    input         wbs_cyc_i,
+    input         wbs_we_i,
+    input  [3:0]  wbs_sel_i,
+    input  [31:0] wbs_dat_i,
+    input  [31:0] wbs_adr_i,
+    output        wbs_ack_o,
     output [31:0] wbs_dat_o,
 
-    // Logic Analyzer Signals
+    // Logic Analyzer
     input  [127:0] la_data_in,
     output [127:0] la_data_out,
     input  [127:0] la_oenb,
 
-    // IOs
+    // Digital IOs
     input  [`MPRJ_IO_PADS-1:0] io_in,
     output [`MPRJ_IO_PADS-1:0] io_out,
     output [`MPRJ_IO_PADS-1:0] io_oeb,
 
-    // Analog (direct connection to GPIO pad---use with caution)
-    // Note that analog I/O is not available on the 7 lowest-numbered
-    // GPIO pads, and so the analog_io indexing is offset from the
-    // GPIO indexing by 7 (also upper 2 GPIOs do not have analog_io).
-    inout [`MPRJ_IO_PADS-10:0] analog_io,
+    // Analog IOs (analog_io[k] <-> GPIO pad k+7)
+    inout  [`MPRJ_IO_PADS-10:0] analog_io,
 
-    // Independent clock (on independent integer divider)
+    // Extra user clock
     input   user_clock2,
 
-    // User maskable interrupt signals
+    // IRQs
     output [2:0] user_irq
 );
 
-/*--------------------------------------*/
-/* User project is instantiated  here   */
-/*--------------------------------------*/
- Neuromorphic_X1_wb mprj(
 
-  // user clock and reset
-           .user_clk(wb_clk_i)           , 
-           .user_rst(wb_rst_i)           ,
-           .wb_rst_i(wb_rst_i)           ,
-            .wb_clk_i(wb_clk_i)           ,
-           .wbs_adr_i(wbs_adr_i)           , // Wishbone write address
-           .wbs_dat_i(wbs_dat_i)               , // Wishbone Read data 
-            .wbs_ack_o(wbs_ack_o)           , // Wishbone acknowledgment
-                .wbs_cyc_i(wbs_cyc_i)           , // Wishbone mode selection
-                .wbs_stb_i(wbs_stb_i)           , // Wishbone mode selection
-                .wbs_we_i(wbs_we_i)            , // Wishbone enable i_wb_we = 1
-        .wbs_sel_i(wbs_sel_i)                 , //
-      .wbs_dat_o(wbs_dat_o)                // Wishbone Write data
-  
+    // -----------------------------
+    // Instantiate your hard macro
+    // -----------------------------
+   Neuromorphic_X1_wb mprj (
+`ifdef USE_POWER_PINS
+  .VDDC (vccd1),
+  .VDDA (vdda1),
+  .VSS  (vssd1),
+`endif
+
+  // Clocks / resets
+  .user_clk (wb_clk_i),
+  .user_rst (wb_rst_i),
+  .wb_clk_i (wb_clk_i),
+  .wb_rst_i (wb_rst_i),
+
+  // Wishbone
+  .wbs_stb_i (wbs_stb_i),
+  .wbs_cyc_i (wbs_cyc_i),
+  .wbs_we_i  (wbs_we_i),
+  .wbs_sel_i (wbs_sel_i),
+  .wbs_dat_i (wbs_dat_i),
+  .wbs_adr_i (wbs_adr_i),
+  .wbs_dat_o (wbs_dat_o),
+  .wbs_ack_o (wbs_ack_o),
+
+  // Scan/Test
+  .ScanInCC  (io_in[0]),
+  .ScanInDL  (io_in[1]),
+  .ScanInDR  (io_in[2]),
+  .TM        (io_in[3]),
+  .ScanOutCC (io_out[0]),
+
+  // Analog / bias pins (drive from analog_io[] wires you already built)
+  .Iref          (analog_io[0]),
+  .Vcc_read      (analog_io[1]),
+  .Vcomp         (analog_io[2]),
+  .Bias_comp2    (analog_io[3]),
+  .Vcc_wl_read   (analog_io[4]),
+  .Vcc_wl_set    (analog_io[5]),
+  .Vbias         (analog_io[6]),
+  .Vcc_wl_reset  (analog_io[7]),
+  .Vcc_set       (analog_io[8]),
+  .Vcc_reset     (analog_io[9]),
+  .Vcc_L         (analog_io[10]),
+  .Vcc_Body      (analog_io[11])
 );
 
 
-endmodule	// user_project_wrapper
-
+endmodule
 `default_nettype wire
+
 
